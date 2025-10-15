9c83917b606bf223d4f7f9b233fa1b043ac0ad4b
MO3K <vredchuk@gmail.com>
2025-10-15 12:44:51 +0300

    test commit

--- google_should_find_selene.py
+++ google_should_find_selene.py
@@ -6,3 +6,4 @@ browser.element('[name="q"]').should(be.blank).type('qa.guru').press_enter()
 browser.element('html').should(have.text('About this page'))
 
 # browser.element('[id="search"]').should(have.text('QA.GURU: Курсы тестировщиков'))
+# test
